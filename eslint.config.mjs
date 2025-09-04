b249f2f92c001d0a1b0cdfa23cffab2e1fbc4289
AlexMRTY <asifrezai261@gmail.com>
2025-09-04 13:55:57 +0200

    Linter and Prettier setup.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,10 +8,24 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    // "plugin:tailwindcss/recommended",
+    "prettier",
+    ""
+  ),
+  {
+    rules: {
+      "no-undef": "off",
+    },
+  },
   {
     ignores: [
       "node_modules/**",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
-export default function Home() {
-  console.log("Hello, Next.js!");
+export default function Home () {
+  console.log('Hello, Next.js!')
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
     </div>
-  );
+  )
 }
