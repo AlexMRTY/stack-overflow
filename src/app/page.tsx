f693228eab928434d9b54ac82faa85fbc144caec
AlexMRTY <asifrezai261@gmail.com>
2025-09-04 14:49:09 +0200

    Add typography plugin and custom fonts; update styles and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-
-export default function Home () {
-  console.log('Hello, Next.js!')
+export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <h1 className="h1-bold">Hello, Next.js! right</h1>
     </div>
-  )
+  );
 }
